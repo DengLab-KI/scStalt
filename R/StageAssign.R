904d925a2de14c4963c2fc5b3188bcc9bd91ffef
Qing Luo <qing.luo@ki.se>
2023-08-15 12:08:48 +0200

    Update StageAssign.R

--- R/StageAssign.R
+++ R/StageAssign.R
@@ -2,7 +2,7 @@
 stageAssign <- function(count, kernel){
     cosine_lineage <- apply(count, 2, similarity,kernel<-kernel)
     cosine_lineage <-  t(cosine_lineage)
-    colnames(cosine_lineage) <- c("lineage_cosine","score_cosine")
+    colnames(cosine_lineage) <- c("cosine_similarity_score","stage_assigned")
     return(cosine_lineage)
 }
 
